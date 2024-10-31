979f5b84886306f04bc6e951564e647592f862e1
Matt Nudds <matthewnudds1@gmail.com>
2024-10-31 11:22:52 +0000

    Completed calculator.java class push method

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,21 @@
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Calculator {
+    private final Deque<Number> stack = new LinkedList<>();
+    private static final List<String> OPERATORS = List.of("+");
+    public void push(Object arg) {
+        if(OPERATORS.contains(arg)){
+            Number y = stack.removeLast();
+            Number x = stack.removeLast();
+            Double answer = null;
+
+            answer = x.doubleValue() + y.doubleValue();
+            push(answer);
+        }
+        else {
+            stack.add((Number) arg);
+        }
+    }
 }
--- src/test/java/AdditionUnitTests.java
+++ src/test/java/AdditionUnitTests.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AdditionUnitTests {
+    @Test
+    public void add_int_1_and_int_2_returns_3(){
+        int a = 1;
+        int b = 2;
+        int expected = 3;
+        Calculator calc = new Calculator();
+        calc.push(a);
+        calc.push(b);
+        calc.push("+");
+        int actualAnswer = calc.value();
+        assertEquals(expected, actualAnswer);
+    }
 }
